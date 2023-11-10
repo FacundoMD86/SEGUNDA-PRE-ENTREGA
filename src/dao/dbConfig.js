f55bc87aa1c62d5bd83a42a7e3214ede0e18a7ce
FacundoMD86 <124894865+FacundoMD86@users.noreply.github.com>
2023-11-10 13:52:14 -0300

    Delete src directory

--- src/dao/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-const URI="mongodb+srv://facundomd:11223344@fmd.cqejc72.mongodb.net/ecommerce?retryWrites=true&w=majority"
-
-await mongoose.connect(URI,{
-    serverSelectionTimeoutMS:5000,
-})
-console.log("Base de datos conectada....")
